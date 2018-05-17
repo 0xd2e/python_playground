ccaa24d133132b6c8a30c84cc53615eafd856416
maciej <f171a9a3497c8b@users.noreply.github.com>
2018-05-17 23:57:56 +0200

    Update freecodecamp_algorithms.py
    User's portfolio page on freecodecamp.org has changed.
    Last two table columns no longer contain hyperlinks to user's answers.
    They contain solution's source code instead
    (which is presented in a modal dialog box).
    
    Main changes:
    -- due to above, dataframe now consists of three columns
    -- add an option to choose a directory where data will be saved
    -- use __file__ instead of argv[0] from sys (import is no longer required)
    -- revise docstrings

--- Web scraping/freecodecamp_algorithms.py
+++ Web scraping/freecodecamp_algorithms.py
@@ -2,15 +2,14 @@
 
 
 from os import path
-from sys import argv
 
 import pandas
-import requests
 
+from requests import get, exceptions
 from bs4 import BeautifulSoup
 
 
-URL = 'https://www.freecodecamp.org'
+_URL = 'https://www.freecodecamp.org'
 
 
 def get_html(username):
@@ -20,36 +19,31 @@ def get_html(username):
     username -- string, valid freecodecamp username
 
 
-    Returns a string, an HTML document with freecodecamp code portfolio
-    of the given user.
+    Returns a string, an HTML document with freecodecamp
+    code portfolio of the given user.
     '''
 
     try:
 
-        full_url = '{}/{}'.format(URL, username)
+        url = '{}/{}'.format(_URL, username)
 
-        response = requests.get(full_url, timeout=3)
+        response = get(url, timeout=3)
 
         response.raise_for_status()
 
-    except requests.exceptions.Timeout as err:
-        print('Cannot get:', full_url)
-        print('Connection timeout')
+    except exceptions.Timeout as err:
+        print('Cannot get:', url)
+        print('Connection timeout', end='\n\n')
 
-    except (requests.exceptions.ConnectionError,
-            requests.exceptions.HTTPError) as err:
-        print('Cannot get:', full_url)
-        print(err)
-
-    except BaseException as err:
-        print(err)
+    except (exceptions.ConnectionError, exceptions.HTTPError) as err:
+        print(err, end='\n\n')
 
     else:
         response.close()
 
-        print('\nSuccessfully get:', full_url)
+        print('\nSuccessfully get:', url)
         print('-- Status code:', response.status_code)
-        print('-- Encoding:', response.encoding)
+        print('-- Encoding:', response.encoding, end='\n\n')
 
         return response.text
 
@@ -61,9 +55,9 @@ def prepare_dataframe(html):
     html -- string, an html document with user's freecodecamp code portfolio
 
 
-    Returns pandas dataframe containing user's solutions to freecodecamp
-    JavaScript programming problems. This dataframe has four columns:
-    Algorithms, Completed, Last Updated, Solution links.
+    Returns pandas dataframe that contain information about user's solutions
+    to freecodecamp JavaScript programming problems. This dataframe has
+    three columns: Algorithms, Completed, Last Updated.
     '''
 
     try:
@@ -73,59 +67,50 @@ def prepare_dataframe(html):
         # Required data is in the first table on the page
         table = soup.find('table')
 
-        # This table has five columns, but the last column contains
-        # duplicated data (links and titles).
+        df = pandas.read_html(
+            table.encode('utf-8'),
+            flavor='lxml',
+            header=0,
+            skiprows=1
+        )[0]
 
-        # Get unique links
-        links = table.find_all('a', href=True)[::2]
-
-        # Create absolute links
-        links = [URL + link['href'] for link in links]
-
-        df = pandas.read_html(table.encode('utf-8'),
-                              flavor='lxml',
-                              header=0,
-                              skiprows=1)[0]
-
-        df.drop(df.columns.values[-1], axis=1, inplace=True)
+        # Last two columns contain solutions source code
+        df.drop(columns=df.columns.values[-2:], inplace=True)
 
         df[df.columns.values[1]] = pandas.to_datetime(df[df.columns.values[1]])
         df[df.columns.values[2]] = pandas.to_datetime(df[df.columns.values[2]])
-        df[df.columns.values[3]] = pandas.Series(links)
 
-        # Rename column
-        df.columns.values[3] += ' links'
-
-    except (TypeError, NameError, AttributeError, IndexError) as err:
-        print('Invalid freecodecamp code portfolio page')
-
-    except BaseException as err:
-        print(err)
+    except (TypeError, NameError, AttributeError, IndexError):
+        print('Invalid freecodecamp code portfolio page', end='\n\n')
 
     else:
-        print('\nDataframe: {} rows, {} columns\n'.format(*df.shape))
+        print('\nDataframe contains {} rows and {} columns\n'.format(*df.shape))
         print(df.dtypes, df.head(), sep='\n\n', end='\n\n')
 
         return df
 
 
-def save_dataframe(df, username, file_type='csv'):
+def save_dataframe(df, username, dir_path='', json_format=False):
     '''
     Inputs:
 
     df -- nonempty pandas dataframe
 
-    username -- string, freecodecamp username (or other user identifier)
+    username
+    -- string, freecodecamp username or other user identifier
 
-    file_type -- string (all lowercase), file format (extension)
-              -- can be either csv or json, default is csv
+    dir_path
+    -- string, path to a directory where data will be saved
+    -- if an empty string is given (default) or the path is invalid,
+       file will be saved to the directory where the script is located
+
+    json_format
+    -- boolean, file format
+    -- save data as JSON if True, as CSV if False (default)
 
 
-    Saves the dataframe in the given format into the directory where
-    the script is located.
-
-
-    This function does not return any value.
+    This function does not return any value,
+    just saves the dataframe to a local drive.
     '''
 
     try:
@@ -135,41 +120,35 @@ def save_dataframe(df, username, file_type='csv'):
 
         assert not df.empty, 'Cannot save an empty dataframe'
 
-        # Ensure correct file extension
-        if file_type not in ('csv', 'json'):
-            print('File format must be either csv or json (all lowercase letters)')
-            print('-- Changing {} to csv\n'.format(file_type))
-            file_type = 'csv'
+        file_type = 'json' if json_format else 'csv'
 
         file_name = 'freecodecamp_{}_solutions.{}'.format(username, file_type)
 
-        file_path = path.dirname(argv[0])
-        file_path = path.abspath(file_path)
+        if not path.isdir(dir_path):
+            dir_path = path.dirname(__file__)
+
+        file_path = path.abspath(dir_path)
         file_path = path.join(file_path, file_name)
 
         with open(file_path, 'wt') as f:
 
-            if file_type == 'json':
+            if json_format:
                 df.to_json(f, orient='records')
             else:
                 df.to_csv(f)
 
     except (IOError, OSError) as err:
         print('Cannot save:', file_path)
+        print(err.strerror if err.strerror else err, end='\n\n')
 
-        if err.strerror:
-            print(err.strerror)
-        else:
-            print(err)
-
-    except (AssertionError, BaseException) as err:
-        print(err)
+    except AssertionError as err:
+        print(err, end='\n\n')
 
     else:
-        print('File saved:', file_path, end='\n\n')
+        print('\nFile saved:', file_path, end='\n\n')
 
 
 if __name__ == '__main__':
-    username = 'f171a9a3497c8b'
-    solutions = prepare_dataframe(get_html(username))
-    save_dataframe(solutions, username)
+    user = 'f171a9a3497c8b'
+    solutions = prepare_dataframe(get_html(user))
+    save_dataframe(solutions, user)
